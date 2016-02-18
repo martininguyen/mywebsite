d8ad091e57f05f7c0ab110fd9f45b9b027f425e9
Martin Nguyen <martinnguyen0618@gmail.com>
2016-02-18 13:44:58 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Module Dependencies
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+var pg = require('pg');
+var handlebars = require('express3-handlebars');
+var path = require('path');
+var data = require('./data.json');
+
+// Environments
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+// app.engine('handlebars', handlebars({defaultLayout : 'master'}));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function(request, response) {
+  response.render('landing');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
